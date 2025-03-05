47ab8877e7f9dee2eb550bd9874e85024ea68ce6
Vihananand <vihananand2018@gmail.com>
2025-03-05 22:32:51 +0530

    updated gallery

--- app/Gallery/page.js
+++ app/Gallery/page.js
@@ -6,7 +6,7 @@ export default function Gallery() {
   return (
     <div className="w-full min-h-screen bg-gradient-to-br from-[#1E3A8A] via-[#1E3A8A]/60 to-[#1E3A8A] font-[helvetica]">
       <section className="text-gray-600 body-font">
-        <div className="w-1/2 px-4 sm:px-6 lg:px-8 py-12 mx-auto">
+        <div className="w-3/4 px-4 sm:px-6 lg:px-8 py-12 mx-auto">
           <div className="flex flex-col items-center text-center space-y-6 mb-12">
             <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-300">
               Our latest Products
@@ -87,7 +87,7 @@ export default function Gallery() {
       </section>
 
       <section className="text-gray-600 body-font">
-        <div className="md:w-1/2 px-4 sm:px-6 lg:px-8 py-12 mx-auto w-full">
+        <div className="md:w-3/4 px-4 sm:px-6 lg:px-8 py-12 mx-auto w-full">
           <div className="flex flex-col items-center text-center space-y-6 mb-12">
             <p className="text-md sm:text-4xl lg:w-2/3 leading-relaxed text-gray-200 uppercase font-bold">
               Textile Machinery Components
@@ -129,7 +129,7 @@ export default function Gallery() {
       </section>
 
       <section className="text-gray-600 body-font">
-        <div className="md:w-1/2 px-4 sm:px-6 lg:px-8 py-12 mx-auto w-full">
+        <div className="md:w-3/4 px-4 sm:px-6 lg:px-8 py-12 mx-auto w-full">
           <div className="flex flex-col items-center text-center space-y-6 mb-12">
             <p className="text-md sm:text-4xl lg:w-2/3 leading-relaxed text-gray-200 uppercase font-bold">
               Tool Room
